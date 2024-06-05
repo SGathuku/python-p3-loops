ff52dc6262be6f51622ce7773799b7c0859beac0
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-05 16:27:30 +0300

    While loop in python

--- lib/looping.py
+++ lib/looping.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
-
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
+    print();
+    
 def square_integers(int_list):
     # code goes here!
     pass
