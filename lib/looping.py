7794f434b72f8fe63f7430912e2afb8ebedea4dc
Gathuku Samwel <gathsam3446@gmail.com>
2024-06-05 16:44:12 +0300

    Conditional statements and loops in python

--- lib/looping.py
+++ lib/looping.py
@@ -6,11 +6,27 @@ def happy_new_year():
         print(counter)
         counter -= 1
     print("Happy New Year!")
-    print();
     
+happy_new_year()
+
+
 def square_integers(int_list):
     return [num**2 for num in int_list]
 
+input_numbers = [4, 7, 14, 34, 78]
+squared_result = square_integers(input_numbers)
+print(squared_result)
+
+
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+fizzbuzz()
